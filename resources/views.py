40e86070d134bfbcec7438f33cf1216258cf8c7a
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-08-30 23:32:09 +0300

    models and templates improved
    created create/edit/list views and templates of post model
    created basic css style

--- resources/views.py
+++ resources/views.py
@@ -1,11 +1,23 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http.request import HttpRequest
 from django.http.response import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
 
 from resources.forms import FileForm
 from resources.models import File, Post
+
+# class UserLoginView(LoginView):
+#     template_name = "resources/registration/login.html"
+
+
+# class UserLogoutView(LogoutView):
+#     template_name = "resources/registration/logout.html"
 
 
 class IndexView(TemplateResponseMixin, View):
@@ -20,11 +32,6 @@ class PostCreateView(CreateView):
     model = Post
     fields = ("title", "content", "is_private", "token")
 
-    def get_context_data(self, **kwargs):
-        a = super().get_context_data(**kwargs)
-        print(a)
-        return a
-
 
 class PostDetailView(DetailView):
     template_name = "resources/post/post_detail.html"
@@ -32,6 +39,10 @@ class PostDetailView(DetailView):
 
     def get_object(self, queryset=None):
         obj: Post = super(PostDetailView, self).get_object(queryset=queryset)
+
+        if self.request.user.is_authenticated:
+            return obj
+
         if obj.is_private:
             url_token = self.request.GET.dict().get("token")
             query = {"token": url_token, "is_enabled": True}
@@ -42,15 +53,25 @@ class PostDetailView(DetailView):
         return obj
 
 
+class PostEditView(UpdateView):
+    template_name = "resources/post/post_create.html"
+    model = Post
+    fields = ("title", "content", "is_private", "token")
+    # success_url = reverse_lazy("post-detail")
+
+    def get_absolute_url(self):
+        return reverse_lazy("post-detail", kwargs={"slug": self.slug})
+
+
+class PostListView(LoginRequiredMixin, ListView):
+    template_name = "resources/post/post_list.html"
+    model = Post
+
+
 class FileCreateView(CreateView):
     template_name = "resources/file/file_create.html"
     model = File
     fields = "__all__"
-
-    def get_context_data(self, **kwargs):
-        a = super().get_context_data(**kwargs)
-        print(a)
-        return a
 
 
 class FileDetailView(DetailView):
