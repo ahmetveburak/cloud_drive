f4346cbc5a1c1e9dc3fa93dbae5a706da282a8f2
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-09-03 22:58:20 +0300

    basic fe design and form customizations for models

--- resources/forms.py
+++ resources/forms.py
@@ -7,6 +7,26 @@ class PostForm(forms.Form):
 
 
 class FileCreateForm(forms.Form):
-    name = forms.CharField(required=False)
-    file = forms.FileField()
+    name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "type": "text",
+                "class": "form-control",
+                "aria-label": "filename",
+                "aria-describedby": "basic-addon1",
+                "placeholder": "Filename is optional",
+            }
+        ),
+    )
+    file = forms.FileField(
+        widget=forms.FileInput(
+            attrs={
+                "type": "file",
+                "class": "custom-file-input",
+                "id": "inputGroupFile01",
+                "aria-describedby": "inputGroupFileAddon01",
+            }
+        )
+    )
     is_private = forms.BooleanField(required=False)
--- tokens/forms.py
+++ tokens/forms.py
@@ -3,12 +3,22 @@ from tempus_dominus.widgets import DateTimePicker
 
 
 class TokenCreateForm(forms.Form):
-    enabled_count = forms.IntegerField(required=False, min_value=0, initial=0)
+    enabled_count = forms.IntegerField(
+        required=False,
+        min_value=0,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"class": "form-control"},
+        ),
+    )
     enabled_to = forms.DateTimeField(
         required=False,
         widget=DateTimePicker(
             options={"useCurrent": True, "collapse": False},
-            attrs={"append": "fa fa-calendar", "icon_toggle": True, "size": "small"},
+            attrs={
+                "append": "fa fa-calendar",
+                "icon_toggle": True,
+            },
         ),
     )
 
