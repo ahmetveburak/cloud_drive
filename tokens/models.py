6c4f4d11ebea5e163bb192062dae6a5a52137ce7
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-09-08 16:29:10 +0300

    refactor if structure, counter def. -1
    getting drunk without drink :D terrible if condition structure fixed.
    counter default to -1 due to new control structure

--- tokens/models.py
+++ tokens/models.py
@@ -7,7 +7,7 @@ class Token(models.Model):
     token = models.CharField(_("Token"), max_length=32, unique=True)
     counter = models.SmallIntegerField(_("View Count"), default=0)
 
-    enabled_count = models.SmallIntegerField(_("View Limit"), default=0)
+    enabled_count = models.SmallIntegerField(_("View Limit"), default=-1)
     enabled_to = models.DateTimeField(_("Expiration Date"), blank=True, null=True)
     is_enabled = models.BooleanField(_("Active Status"), default=True)
 
@@ -17,23 +17,16 @@ class Token(models.Model):
         return f"{self.id} | {self.token[:10]}"
 
     def is_accessible(self) -> bool:
-        if not self.is_enabled:
-            return False
-
-        if self.enabled_count == 0 and not self.enabled_to:
-            return True
-
-        if self.counter < self.enabled_count:
-            self.counter += 1
-            if self.counter == self.enabled_count:
+        if self.is_enabled:
+            time_delta = (self.enabled_to - timezone.now()).days
+            if self.enabled_to and time_delta < 0:
                 self.is_enabled = False
-            self.save()
-            return True
+                self.save()
 
-        if self.enabled_to:
-            if (self.enabled_to - timezone.now()).days >= 0:
-                return True
-            self.is_enabled = False
-            self.save()
+            if self.counter < self.enabled_count:
+                self.counter += 1
+                self.is_enabled = not (self.counter == self.enabled_count)
+                self.save()
 
+            return self.is_enabled
         return False
